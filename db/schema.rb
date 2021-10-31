4373f55e412f0014981159ba9a14c6c755d0f1ff
pentpen <21038372+pentpen@users.noreply.github.com>
2021-10-31 22:02:10 +0900

    remigrate, rename some english words to japanese words

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_044922) do
+ActiveRecord::Schema.define(version: 2021_10_31_125121) do
 
   create_table "labels", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_10_31_044922) do
     t.string "title"
     t.string "content"
     t.date "reported"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "work_items", force: :cascade do |t|
+    t.string "name"
+    t.string "label"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
