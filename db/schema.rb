327ca5d426d722c728f1d684c1365086a15812eb
pentpen <21038372+pentpen@users.noreply.github.com>
2021-10-31 13:51:16 +0900

    create label(model)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_042235) do
+ActiveRecord::Schema.define(version: 2021_10_31_044922) do
+
+  create_table "labels", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reports", force: :cascade do |t|
     t.string "title"
