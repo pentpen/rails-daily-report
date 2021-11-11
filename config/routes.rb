6d100918bf0511b611043996ee097b4d52c1895e
pentpen <21038372+pentpen@users.noreply.github.com>
2021-11-11 17:01:39 +0900

    Revert "Feature for heroku (#1)"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :work_items
   resources :labels
   resources :reports
-  root 'reports#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
