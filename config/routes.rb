e6f42ff087d037b31b7edd967d8a2cc0652e9bf1
pentpen <21038372+pentpen@users.noreply.github.com>
2021-11-12 22:59:44 +0900

    add json response to work_items

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :reports
   root 'reports#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    get '/work_items', to: 'work_items#index', defaults: { format: :json }
+    get '/work_items/:id', to: 'work_items#show', defaults: { format: :json }
+  end
 end
